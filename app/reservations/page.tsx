05c2a14f18beead626579e29edfa04d77c63310f
Vincent Lacoste <vinzlac@gmail.com>
2025-02-10 13:52:35 +0100

    dans la page des réservations ajout des fleches de navigation gauche droite ou diminuer ou augmenter d'un jour

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect, Suspense } from 'react';
 import { Reservation } from '@/app/types/reservation';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
-import { format } from 'date-fns';
+import { format, addDays, subDays } from 'date-fns';
 import { fr } from 'date-fns/locale';
 
 interface ReservationByTimeSlot {
@@ -21,6 +21,9 @@ function ReservationsContent() {
 
   // Formatage de la date
   const formattedDate = date ? format(new Date(date), "EEEE d MMMM yyyy", { locale: fr }) : '';
+
+  const previousDate = format(subDays(new Date(date), 1), 'yyyy-MM-dd');
+  const nextDate = format(addDays(new Date(date), 1), 'yyyy-MM-dd');
 
   useEffect(() => {
     const fetchReservations = async () => {
@@ -70,19 +73,27 @@ function ReservationsContent() {
   return (
     <div className="min-h-screen p-8 pb-20 sm:p-20">
       <div className="max-w-7xl mx-auto">
-        <Link
-          href="/"
-          className="inline-flex items-center text-blue-600 hover:text-blue-800 mb-8"
-        >
-          <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-          </svg>
-          Retour
-        </Link>
-
-        <h1 className="text-3xl font-bold mb-8">
-          Réservations pour la date du {formattedDate}
-        </h1>
+        <div className="flex items-center justify-between mb-8">
+          <Link
+            href={`/reservations?date=${previousDate}`}
+            className="text-blue-600 hover:text-blue-800"
+          >
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+            </svg>
+          </Link>
+          <h1 className="text-3xl font-bold">
+            Réservations pour la date du {formattedDate}
+          </h1>
+          <Link
+            href={`/reservations?date=${nextDate}`}
+            className="text-blue-600 hover:text-blue-800"
+          >
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+            </svg>
+          </Link>
+        </div>
 
         {loading ? (
           <div className="flex justify-center items-center h-40">
