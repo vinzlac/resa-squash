d9e354951530813effdefee3faa86dd7544cde49
Vincent Lacoste <vinzlac@gmail.com>
2025-02-15 14:29:35 +0100

    fix deploy vercel - Type error: Cannot find name 'useEffect'.

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, Suspense } from 'react';
+import { useState, useEffect, Suspense } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useUserStore } from '../../stores/userStore';
 
