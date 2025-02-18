cf9ee55ab334158fb4b600b0682427688884f252
Vincent Lacoste <vinzlac@gmail.com>
2025-02-18 21:23:16 +0100

    clean teamR api_key

--- app/services/config.ts
+++ app/services/config.ts
@@ -1,7 +1,7 @@
-export const AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InZpbnpsYWNAZ21haWwuY29tIiwidXNlclR5cGUiOiJhcHAiLCJ1c2VySWQiOiI2MGJmMmZkZDFmZDhkMjAwMjBkMmM4YTciLCJjb2FjaCI6ZmFsc2UsImN1c3RvbUlkIjoiNWRkNmIzOTYxNTEwYzkxZDM1M2IwODMzIiwiaWF0IjoxNzIyOTU1NDkyLCJleHAiOjE3NTQ0OTE0OTJ9.TwjtkykWXXaTSFB3zq12qBVCROZuU5dSDxHq7Hp9n4c";
 export const GET_LICENSEE_URL = "https://app.teamr.eu/users/licensees";
 export const BOOKING_URL = 'https://app.teamr.eu/sessions/book/twoLicenseesFromClub';
 export const TEAMR_AUTH_URL = 'https://app.teamr.eu/users/custom/authenticate/v2';
+export const GET_SESSION_URL = "https://app.teamr.eu/nearfilters/clubId";
 
 export const COURT_CLUB_IDS: { [key: string]: string } = {
   "1": "60b754170ebdd0002094521b",
@@ -9,17 +9,6 @@ export const COURT_CLUB_IDS: { [key: string]: string } = {
   "3": "60b77145e704970027492488",
   "4": "60b771610ebdd0002094562d"
 };
-export const GET_SESSION_URL = "https://app.teamr.eu/nearfilters/clubId";
 export const CUSTOM_ID = "5dd6b3961510c91d353b0833";
 export const COORDINATES = [2.5864862369264747, 48.869659697477495];
 
-export const USER_ID = "60bf2fdd1fd8d20020d2c8a7";
-
-export const HEADERS = {
-  "Host": "app.teamr.eu",
-  "Accept": "application/json, text/plain, */*",
-  "User-Agent": "HappyPeople/201 CFNetwork/1568.200.51 Darwin/24.1.0",
-  "Accept-Language": "fr-FR,fr;q=0.9",
-  "Authorization": AUTH_TOKEN,
-  "Content-Type": "application/json"
-}; 
