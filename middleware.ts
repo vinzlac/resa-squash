676ac6aea958772553b0cc2b64a04c011d1d4737
Vincent Lacoste <vinzlac@gmail.com>
2025-02-18 18:42:04 +0100

    remove unused imports

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,5 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { jwtDecode } from 'jwt-decode';
-import { DecodedToken } from '@/app/types/auth';
 
 const publicPaths = ['/login', '/unauthorized'];
 
